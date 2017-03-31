73653bdda718a46f09564283b785ce18bff55888
jalvarez <j@alvarez>
2017-03-31 12:53:30 -0300

    Pre commit
    Change-Id: I38339f2bcd4c758c58a1049b9688ee3a935096ab
    Signed-off-by: jalvarez

---
+++ src/main/java/com/superencasa/rest/RESTfulSapoPepe.java
@@ -0,0 +1,32 @@
+package com.superencasa.rest;
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+ 
+@Path("/sappo")
+public class RESTfulSapoPepe 
+{
+	/*
+	@GET
+	@Produces("text/html")
+	public Response getStartingPage()
+	{
+		String output = "<h1>Sapo Pepe<h1>" +
+				"<p>RESTful Service is runeando ... <br>Ping @ " + new Date().toString() + "</p<br>";
+		return Response.status(200).entity(output).build();
+	}
+	*/
+	
+	@Path("/saludo/{c}/{d}")
+	@GET
+	@Produces("text/html")
+	public Response saludo(@PathParam("c") String c, @PathParam("d") String d) {
+		String output = "<h1>" + c + " " + d + "<h1>" +
+				"<p>RESTful Service is runeando ... <br> " + new Date().toString() + "</p<br>";
+		return Response.status(200).entity(output).build();
+	}
+}
